485cd1770960ae52a1a5e76316b4c20a1ae4f9f7
daniel <danielbaradaran31-hash>
2025-12-03 02:29:09 +0100

    <p>14C

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
             <article className="hourly-card">
               <p className="hour">16:00</p>
               <img src="./public/image/sunny.png" />
-              <p className="hour-temp">27C</p>
+              <p className="hour-temp">14C</p>
             </article>
             <article className="hourly-card">
               <p className="hour">16:00</p>
